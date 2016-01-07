c7b4d1cad2ee5c4848396e498888496dd78032ac
Mihail Podshivadlov <m.podshivadlov@outlook.com>
2016-01-07 23:39:20 +0300

    Included MainWindowViewModel

---
+++ HostsBlocker/ViewsModels/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using HostsBlocker.Annotations;
+using HostsBlocker.Models;
+
+namespace HostsBlocker.ViewsModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private HostsModel hosts;
+
+        public HostsModel Hosts
+        {
+            get { return this.hosts; }
+            set
+            { 
+                this.hosts = value;
+                this.OnPropertyChanged(nameof(this.Hosts));
+            }
+        }
+
+        //public void AddHost(HostInfoModel host)
+        //{
+        //    this.hosts.Add(host);
+        //    //this.OnCollectionChanged(NotifyCollectionChangedAction.Add, nameof(this.Hosts));
+        //}
+
+        //public bool RemoveHost(HostInfoModel host)
+        //{
+        //    var result = this.hosts.Remove(host);
+        //    //if (result)
+        //    //    this.OnCollectionChanged(NotifyCollectionChangedAction.Remove, nameof(this.Hosts));
+        //    return result;
+        //}
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
